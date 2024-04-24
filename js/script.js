6ede96ea249a80df3ffeb4cde30f2cf494aa82e2
LukeDitchWalker1 <lucasjhatch@gmail.com>
2024-04-23 19:42:26 -0600

    modified:   css/styles.css 	modified:   html/index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
-var version = document.getElementById("version")
-var texturepack = document.getElementById("Texture Pack")
-
 function Launch() {
-    if (version == "none" || texturepack == "none") {
-        window.alert("Please confirm your settings!")
-    } else {
-        
-        if (version == "Vanilla1.8.8") {
-            var x = document.createElement("IFRAME");
-            x.setAttribute("src", "../versions/EaglercraftX_1.8_Offline_International.html");
-            document.body.appendChild(x);
-        } else if (version == "Resent1.8.8") {
-            var x = document.createElement("IFRAME");
-            x.setAttribute("src", "../versions/index.html");
-            document.body.appendChild(x);
-        }
-        
-    }
+    var version = document.getElementById("version").value;
+    var texturepack = document.getElementById("Texture Pack").value;
 
-} 
+    if (version == "none" || texturepack == "none") {
+        window.alert("Please confirm your settings!");
+    } else {
+        var iframe = document.createElement("iframe");
+        iframe.className = "fullscreen-iframe";
+        if (version == "Vanilla1.8.8") {
+            iframe.setAttribute("src", "../versions/vanilla.html");
+        } else if (version == "Resent1.8.8") {
+            iframe.setAttribute("src", "../versions/resent.html");
+        }
+        document.body.appendChild(iframe);
+    }
+}
